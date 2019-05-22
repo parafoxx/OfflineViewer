255a7fbfd5359f8e68b0a8a35e910639eb258e35
s0561440 <tom-morelly@gmx.de>
2019-05-22 20:25:15 +0200

    implemented buttom nav bar in every activity. TODO: rename activies, according navbar, fix navigaiton onCreate

--- app/src/main/java/de/htwBerlin/ois/Activities/MainActivity.java
+++ app/src/main/java/de/htwBerlin/ois/Activities/MainActivity.java
@@ -7,9 +7,12 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import org.mapsforge.core.model.LatLong;
@@ -24,13 +27,17 @@ import org.mapsforge.map.android.view.MapView;
 
 import java.io.File;
 
+import butterknife.BindView;
 import de.htwBerlin.ois.FileStructure.MapFileSingleton;
+import de.htwBerlin.ois.R;
 
 public class MainActivity extends Activity {
 
     private static final String MAP_FILE = "berlin.map";
     private static final String TAG = "MainActivity";
     public static final int READ_EXTERNAL_STORAGE = 112;
+
+    @BindView(R.id.bottom_navigation) BottomNavigationView bottom_navigation;
 
     private MapView mapView = null;
     private File mapFile;
@@ -64,6 +71,33 @@ public class MainActivity extends Activity {
         mapView.getLayerManager().getLayers().add(tileRendererLayer);
         mapView.setCenter(new LatLong(52.517037, 13.38886));
         mapView.setZoomLevel((byte) 12);
+
+        Menu menu = bottom_navigation.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+
+        bottom_navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.nav_about:
+                        Intent aboutIntent = new Intent(MainActivity.this, AboutActivity.class);
+                        startActivity(aboutIntent);
+                        break;
+                    case R.id.nav_download:
+                        Intent navigationIntent = new Intent(MainActivity.this, MapDowloadActivity.class);
+                        startActivity(navigationIntent);
+                        break;
+                    case R.id.nav_home:
+                        Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
+                        startActivity(startIntent);
+                        break;
+                }
+                return false;
+            }
+        });
+
     }
 
     @Override
